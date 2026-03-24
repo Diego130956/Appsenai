6fd679569c7993d0e046a28ed91edf2e79e7c3f2
senai_25_26 <diegobertaglia450@gmail.com>
2026-03-24 13:23:50 -0300

    Atividade mobile

--- src/app.py
+++ src/app.py
@@ -1,9 +1,9 @@
 from turtle import width
 
 import flet
-from flet import ThemeMode, Text, TextField, OutlinedButton, Column, CrossAxisAlignment, ElevatedButton, TextButton
+from flet import ThemeMode, Text, TextField, OutlinedButton, Column, CrossAxisAlignment, ElevatedButton, Container, \
+    TextButton, Colors, FontWeight
 from datetime import datetime
-
 
 
 def main(page: flet.Page):
@@ -12,28 +12,23 @@ def main(page: flet.Page):
     page.window.width = 400
     page.window.height = 700
 
-
     def salvar_nome():
         text.value = f'Bom Dia {input_nome.value} {input_sobrenome.value}'
 
     def verificar_parimpar():
-        numero = input_numero.value
-        if numero % 2 == 0:
-            text_parimapr.value = f'O {numero} é par'
+        numero1 = int(input_numero.value)
+        if numero1 % 2 == 0:
+            text_parimapr.value = f'O {numero1} é par'
         else:
-            text_parimapr.value = f'O {numero} é impar'
+            text_parimapr.value = f'O {numero1} é impar'
 
     def calcular_idade():
         numero = input_idade.value
         idade = datetime.now().year - int(numero)
-        if  idade >= 18:
+        if idade >= 18:
             text_idade.value = f'Você tem {idade} e é maior de idade'
         else:
             text_idade.value = f'Você tem {idade} e é menor de idade'
-
-
-
-
 
     text = Text()
     text_parimapr = Text()
@@ -43,28 +38,84 @@ def main(page: flet.Page):
     input_numero = TextField(label="Digite um numero", hint_text="Verifique se é par ou impar")
     input_idade = TextField(label="Digite o ano de nascimento", hint_text="EX: 2000")
     btn_salvar = OutlinedButton("Salvar", on_click=salvar_nome)
-    btn_verificar = ElevatedButton("Verificar", on_click=salvar_nome)
+    btn_verificar = ElevatedButton("Verificar", on_click=verificar_parimpar)
     btn_calcular = TextButton("Calcular", on_click=calcular_idade)
 
     page.add(
-        Column(
-            [
-                input_nome,
-                input_sobrenome,
-                btn_salvar,
-                text,
-                input_numero,
-                btn_verificar,
-                text_parimapr,
-                input_idade,
-                btn_calcular,
-                text_idade,
+
+                Container(
+                    Column(
+                        [
+                            Text("Atividade 01", weight=FontWeight.BOLD),
+                            input_nome,
+                            input_sobrenome,
+                            btn_salvar,
+                            text,
 
 
-            ],
-            width = 400,
-            horizontal_alignment = CrossAxisAlignment.CENTER
-        )
+
+                        ],
+
+                        horizontal_alignment=CrossAxisAlignment.CENTER
+
+
+
+                    ),
+                    bgcolor=Colors.BLUE_300,
+                    padding=15,
+                    width = 400,
+                    border_radius=10
+
+                ),
+                Container(
+                    Column(
+                        [
+                            Text("Atividade 02", weight=FontWeight.BOLD),
+
+                            input_numero,
+                            btn_verificar,
+                            text_parimapr
+
+                        ],
+
+                        horizontal_alignment=CrossAxisAlignment.CENTER
+
+                    ),
+                    bgcolor=Colors.RED_300,
+                    padding=15,
+                    width=400,
+                    border_radius = 10
+
+                ),
+                Container(
+                    Column(
+                        [
+                            Text("Atividade 03", weight=FontWeight.BOLD),
+
+                            input_idade,
+                            btn_calcular,
+                            text_idade
+
+                        ],
+
+                        horizontal_alignment=CrossAxisAlignment.CENTER
+
+                    ),
+                    bgcolor=Colors.PURPLE_300,
+                    padding=15,
+                    width=400,
+                    border_radius=10
+
+                )
+
+
+
     )
 
+
 flet.run(main)
+
+
+# input_idade,
+#                             btn_calcular,
+#                             text_idade,
